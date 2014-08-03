60085e18936432964ccf37e611f96a91db508f3c
Di Huynh <di.huynh@cs.utexas.edu>
2014-08-02 19:05:00 -0500

    change fonts for about page

--- js/logic.js
+++ js/logic.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+
+	// ------------------------- SPLASH PAGE ------------------------- //
 	$('.greeting').addClass('animated fadeInRight');
+
+
+	// ------------------------- ABOUT PAGE ------------------------- //
+	$('#slidemarginleft button').click(function() {
+		var $marginLefty = $(this).next();
+		$marginLefty.animate({
+			marginLeft: parseInt($marginLefty.css('marginLeft'),10) 
+									== 0 ? $marginLefty.outerWidth() :0
+		});
+	});
 
 });
