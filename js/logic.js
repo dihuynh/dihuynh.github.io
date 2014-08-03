c87bfa3576d73c3912095d585ead67ed5eb04d9a
Di Huynh <di.huynh@cs.utexas.edu>
2014-08-02 20:26:36 -0500

    added glyicons

--- js/logic.js
+++ js/logic.js
@@ -5,12 +5,6 @@ $(document).ready(function(){
 
 
 	// ------------------------- ABOUT PAGE ------------------------- //
-	$('#slidemarginleft button').click(function() {
-		var $marginLefty = $(this).next();
-		$marginLefty.animate({
-			marginLeft: parseInt($marginLefty.css('marginLeft'),10) 
-									== 0 ? $marginLefty.outerWidth() :0
-		});
-	});
+	
 
 });
