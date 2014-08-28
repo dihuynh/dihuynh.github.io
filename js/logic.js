b342ec327b8d7c42c3296cbc9171ea21aa9f6435
Di Huynh <dihuynh@cs.utexas.edu>
2014-08-27 21:16:19 -0500

    move code to the right page

--- js/logic.js
+++ js/logic.js
@@ -1,38 +1,42 @@
+    
+
     var app = angular.module('slideShow', []);
     console.log("initialized the module");
     app.controller('SlidesController', function(){
         this.slides = pictures;
     });
+
     console.log("after controller constructor");
     var pictures = [
-        {
-            description: 'I met Pentatonix',
-            imgLink: 'img/f.jpg'
-        }, {
-            description: 'I met Jon Cozart',
-            imgLink: 'img/e.jpg'
-        }, {
-            description: 'I told Emeli Sande her show was amazing and she said thanks',    
-            imgLink: 'img/g.jpg'
-        }, {
-            description: 'I jumped a cliff',
-            imgLink: 'img/d.jpg'
-        }, {
-            description: 'I love coffee',
-            imgLink: 'img/a.jpg'
-        },{
-            description: 'I\'m in an unofficial ukulele trio',
-            imgLink: 'img/c.jpg'
-        },{
-            description: 'I met Jeremy Lin',
-            imgLink: 'img/b.jpg'
-        },{
-            description: 'I attended a talk by Pooja Sankar',
-            imgLink: 'img/h.jpg'
-        },{
-            description: 'I met Chandler Parsons',
-            imgLink: 'img/i.jpg'
-        },{
-            description: 'I love dogs',
-            imgLink: 'img/j.jpg'
-        }];
+    {
+        description: 'I met Pentatonix',
+        imgLink: 'img/f.jpg'
+    }, {
+        description: 'I met Jon Cozart',
+        imgLink: 'img/e.jpg'
+    }, {
+        description: 'I told Emeli Sande her show was amazing and she said thanks',    
+        imgLink: 'img/g.jpg'
+    }, {
+        description: 'I jumped a cliff',
+        imgLink: 'img/d.jpg'
+    }, {
+        description: 'I love coffee',
+        imgLink: 'img/a.jpg'
+    },{
+        description: 'I\'m in an unofficial ukulele trio',
+        imgLink: 'img/c.jpg'
+    },{
+        description: 'I met Jeremy Lin',
+        imgLink: 'img/b.jpg'
+    },{
+        description: 'I attended a talk by Pooja Sankar',
+        imgLink: 'img/h.jpg'
+    },{
+        description: 'I met Chandler Parsons',
+        imgLink: 'img/i.jpg'
+    },{
+        description: 'I love dogs',
+        imgLink: 'img/j.jpg'
+    }];
+    var stuff = [1,2,3,4];
