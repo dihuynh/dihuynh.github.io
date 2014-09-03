e3cb7d35822c3954f4ae16fff9a4e8c11679c148
Di Huynh <dihuynh@cs.utexas.edu>
2014-09-02 23:22:20 -0500

    change styles

--- js/logic.js
+++ js/logic.js
@@ -12,6 +12,9 @@ $(document).ready(function(){
     $('#social-media').toggle(options);
   });
 
+  $('#nextButton').click(function(){
+    location.reload();
+  });
   // ------------------------- MISC PAGE ------------------------- //
   // var app = angular.module('slideShow', []);
   // // console.log("initialized the module");
@@ -22,38 +25,38 @@ $(document).ready(function(){
   // console.log("after controller constructor");
 
 });
-  var pictures = [
-    {
-      description: 'I met Pentatonix',
-      imgLink: 'img/f.jpg'
-    }, {
-      description: 'I met Jon Cozart',
-      imgLink: 'img/e.jpg'
-    }, {
-      description: 'I told Emeli Sande her show was amazing and she said thanks',    
-      imgLink: 'img/g.jpg'
-    }, {
-      description: 'I jumped off a cliff',
-      imgLink: 'img/d.jpg'
-    }, {
-      description: 'I love coffee',
-      imgLink: 'img/a.jpg'
-    },{
-      description: 'I\'m in an unofficial ukulele trio',
-      imgLink: 'img/c.jpg'
-    },{
-      description: 'I met Jeremy Lin',
-      imgLink: 'img/b.jpg'
-    },{
-      description: 'I attended a talk by Pooja Sankar',
-      imgLink: 'img/h.jpg'
-    },{
-      description: 'I met Chandler Parsons',
-      imgLink: 'img/i.jpg'
-    },{
-      description: 'I love dogs',
-      imgLink: 'img/j.jpg'
-  }];
+var pictures = [
+{
+  description: 'I met Pentatonix',
+  imgLink: 'img/f.jpg'
+}, {
+  description: 'I met Jon Cozart',
+  imgLink: 'img/e.jpg'
+}, {
+  description: 'I told Emeli Sande her show was amazing and she said thanks',    
+  imgLink: 'img/g.jpg'
+}, {
+  description: 'I jumped off a cliff',
+  imgLink: 'img/d.jpg'
+}, {
+  description: 'I love coffee',
+  imgLink: 'img/a.jpg'
+},{
+  description: 'I\'m in an unofficial ukulele trio',
+  imgLink: 'img/c.jpg'
+},{
+  description: 'I met Jeremy Lin',
+  imgLink: 'img/b.jpg'
+},{
+  description: 'I attended a talk by Pooja Sankar',
+  imgLink: 'img/h.jpg'
+},{
+  description: 'I met Chandler Parsons',
+  imgLink: 'img/i.jpg'
+},{
+  description: 'I love dogs',
+  imgLink: 'img/j.jpg'
+}];
 
   // return the list element at a random index
   function pickRandom(){
@@ -66,3 +69,4 @@ $(document).ready(function(){
     $scope.slides = pictures;
     $scope.pickRandom = pickRandom();
   }
+
