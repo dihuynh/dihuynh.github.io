7830a984597ac729725967d0d25bcd64c967bb89
Di Huynh <di.huynh@blackbaud.com>
2015-04-18 20:03:26 -0500

    remove tacky stuff

--- js/logic.js
+++ js/logic.js
@@ -3,14 +3,6 @@ $(document).ready(function(){
 
   // ------------------------- SPLASH PAGE ------------------------- //
   $('.greeting').addClass('animated fadeInRight');
-
-  // ------------------------- ABOUT PAGE ------------------------- //
-  $('.next-arrow').click(function(){
-    var options = {direction: 'right', easing:'linear'};
-    var duration = 500;
-    $('#social-media').toggle(options);
-  });
-
 });
 
 
